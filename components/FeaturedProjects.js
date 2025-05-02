e1cfb5b46173a95ced3f376890b7f1949e73b78b
sabareh <sabarevictor@gmail.com>
2025-05-02 13:59:43 +0300

    NecessaryChanges

--- components/FeaturedProjects.js
+++ components/FeaturedProjects.js
@@ -1,15 +1,9 @@
 import { styled } from '../stitches.config'
 
 export const FeaturedProjects = styled('div', {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(1, 1fr)',
-  gap: '2rem',
-  marginTop: '2rem',
-
-  '@bp2': {
-    gridTemplateColumns: 'repeat(2, 1fr)',
-  },
-  '@bp3': {
-    gridTemplateColumns: 'repeat(3, 1fr)',
-  },
+  margin: '10px 0 0 -20px',
+  display: 'flex',
+  flexWrap: 'wrap',
+  flexDirection: 'column',
+  '@bp2': { flexDirection: 'row' },
 })
