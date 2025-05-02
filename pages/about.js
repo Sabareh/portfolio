461c45f6b1e710614905bc4cbad86e34f8d65e6b
sabareh <sabarevictor@gmail.com>
2025-05-02 11:16:51 +0300

    NecessaryChanges

--- pages/about.js
+++ pages/about.js
@@ -59,8 +59,7 @@ function About(props) {
               '@bp2': { marginTop: '-6px' },
             }}
           >
-            <strong>Hey, I'm Victor Sabare</strong>
-            <Pronunciation />I once bricked an iPod at 13 while jailbreaking
+            <strong>Hey, I'm Victor Sabare</strong> I once bricked an iPod at 13 while jailbreaking
             it—igniting my lifelong obsession with tech. Now, I transform raw
             data into meaningful impact.
           </Paragraph>
