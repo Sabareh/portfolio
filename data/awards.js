fa120695da85be04e96563218a976e5c431dd561
sabareh <sabarevictor@gmail.com>
2025-05-02 12:21:00 +0300

    NecessaryChanges

--- data/awards.js
+++ data/awards.js
@@ -8,10 +8,11 @@ const awards = [
         date: 'Apr 2022 - Present · 3 yrs 2 mos',
         by: 'Microsoft',
         summary:
-          'Collaborated with a diverse team of fellow ambassadors to promote Microsoft technologies and learning resources within the student community. Organized and executed engaging workshops, webinars, and events to educate peers about the latest Microsoft technologies and tools. Developed and delivered technical content, presentations, and demos on topics such as Azure, Microsoft 365, and Power Platform. Mentored and provided guidance to students interested in pursuing careers in technology, offering insights into industry trends and opportunities. Created and managed social media content to raise awareness about Microsoft Learn initiatives, resulting in increased engagement and participation. Actively contributed to community‑building efforts by fostering connections between students, educators, and industry professionals. Collaborated with Microsoft representatives to facilitate access to exclusive resources and opportunities for fellow students. Represented Microsoft Learn at local and regional conferences, showcasing the benefits of the program and inspiring others to join. Continuously updated skills and knowledge in line with Microsoft technologies, staying current with the latest updates and advancements.',
-      },
+          'Collaborated with a diverse team of fellow ambassadors to promote Microsoft technologies and learning resources within the student community. Organized and executed engaging workshops, webinars, and events to educate peers about the latest Microsoft technologies and tools. Developed and delivered hands-on coding sessions, hackathons, and tech talks to foster a culture of learning and innovation among students. Actively engaged with the Microsoft community to share knowledge, resources, and best practices.'
+      }
     ],
   },
 ]
 
 export default awards
+
