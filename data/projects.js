62ea7bcd9dbc846beddd47b9d46e1011135fc1c1
sabareh <sabarevictor@gmail.com>
2025-05-15 07:48:44 +0300

    FIxing the errors on projects data page

--- data/projects.js
+++ data/projects.js
@@ -62,7 +62,7 @@ const items = [
         active: false,
         icon: 'product-network',
         stats: 'R · Shiny · Network Analysis',
-      }
+      },
       {
         title: 'Stock Prediction with Spark & Cassandra',
         description: 'This is a data pipeline for predicting stock prices using Apache Spark, Apache Cassandra, and machine learning techniques. It collects and preprocesses stock data from Alpha Vantage API, engineers features, trains models, and performs data analysis and predictions.',
